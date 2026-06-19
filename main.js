fa71f1a0348eec623e13eab010282284ca683006
Ainur Rozikin <rozikinainur742@gmail.com>
2026-06-19 09:36:17 +0700

    melanjutkan belajar konsep OOP JS seperthi extend, super, dan new

--- main.js
+++ main.js
@@ -5,58 +5,91 @@ tanpa menggunakan kata kunci new atau class.
 
 */
 
-function createColor(r, g, b) {
-    return {
-        r, g, b,
-        hex() {
-            return '#' + ((1 << 24) + (r << 8).toString(16).slice(1));
-        },
-        rgb() {
-            return `rgb(${r}, ${g}, ${b})`
-        }
-    };
+// function createColor(r, g, b) {
+//     return {
+//         r, g, b,
+//         hex() {
+//             return '#' + ((1 << 24) + (r << 8).toString(16).slice(1));
+//         },
+//         rgb() {
+//             return `rgb(${r}, ${g}, ${b})`
+//         }
+//     };
+// }
+
+// const red = createColor(255, 0, 0);
+// const green = createColor(0, 255, 0);
+
+// console.log(red.hex()); // #ff0000
+// console.log(green.rgb()); // rgb(0, 255, 0)
+
+
+// // mendefinisikan object dengan class
+// class color {
+//     constructor(r,g,b){
+//         this.r = r;
+//         this.g = g;
+//         this.b = b;
+//     }
+
+
+
+//     rgb(){
+//         const {r, g, b} = this;
+//         return `rgb(${r},${g},${b})`;
+//     }
+
+// }
+// let kuning = new color(255, 196, 0);
+// document.body.style.backgroundColor = kuning.rgb();
+
+
+
+// class Game {
+//     constructor(nama, peran, darah){
+//         this.nama = nama;
+//         this.peran = peran;
+//         this.darah = darah;
+//     }
+
+//     aksi(){
+//         let {nama, peran, darah} = this;
+//         return `${nama} sedang melakukan penyerangan`;
+//     }
+// }
+
+// const hero1 = new Game('furina','support',15000);
+// console.log(hero1.aksi());
+
+
+// konsep inheritence atau pewarisan sifat dalam javascript
+
+
+// ini parent class
+class Kendaraan { 
+    constructor(merek){
+        this.merek = merek;
+    }
+
+    jalan() {
+        return 'Berputar Rodanya.';
+    }
 }
 
-const red = createColor(255, 0, 0);
-const green = createColor(0, 255, 0);
-
-console.log(red.hex()); // #ff0000
-console.log(green.rgb()); // rgb(0, 255, 0)
-
-
-// mendefinisikan object dengan class
-class color {
-    constructor(r,g,b){
-        this.r = r;
-        this.g = g;
-        this.b = b;
+// ini child class
+class Mobil extends Kendaraan { 
+    constructor(merek, model) {
+        // 1. Oper merek ke parent, lalu buat properti model untuk child
+        super(merek); 
+        this.model = model; 
     }
 
-
-
-    rgb(){
-        const {r, g, b} = this;
-        return `rgb(${r},${g},${b})`;
-    }
-
-}
-let kuning = new color(255, 196, 0);
-document.body.style.backgroundColor = kuning.rgb();
-
-
-
-class Game {
-    constructor(nama, peran, darah){
-        this.nama = nama;
-        this.peran = peran;
-        this.darah = darah;
-    }
-
-    aksi(){
-        let {nama, peran, darah} = this;
-        return `${nama} sedang melakukan penyerangan`;
+    aksi() {
+        // 2. Panggil method parent dengan super.jalan(), lalu gabung string-nya
+        return `kendaraan mobil dengan merek ${this.merek} sedang ${super.jalan()}`;
     }
 }
 
-const hero1 = new game('furina','support',15000);
-console.log(hero1.aksi());
+const mobilSaya = new Mobil('Toyota', 'Avanza');
+console.log(mobilSaya.aksi());
+
